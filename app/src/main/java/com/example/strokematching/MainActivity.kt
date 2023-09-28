19822573339a3fea26a687e65eb464aa24baf56d
Ganang Arief Pratama <ganang.pratama@dana.id>
2023-09-28 19:48:22 +0700

    add paint view and logic to show default strokes

--- app/src/main/java/com/example/strokematching/MainActivity.kt
+++ app/src/main/java/com/example/strokematching/MainActivity.kt
@@ -1,11 +1,103 @@
 package com.example.strokematching
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Path
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import com.example.strokematching.PaintView.Companion.defaultStrokes
+import com.example.strokematching.PaintView.Companion.jsonPoints
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
+import com.google.gson.stream.JsonReader
+import java.io.BufferedWriter
+import java.io.File
+import java.io.FileReader
+import java.io.FileWriter
+import java.io.Writer
+import java.lang.reflect.Type
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var saveButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        configureComponent()
+        setupListenerComponent()
+
+        readJSON()
+    }
+
+    private fun configureComponent() {
+        saveButton = findViewById(R.id.save_button)
+    }
+
+    private fun setupListenerComponent() {
+        saveButton.setOnClickListener {
+            savePointsToJSON()
+        }
+    }
+
+    private fun savePointsToJSON() {
+        val gsonPretty = GsonBuilder().setPrettyPrinting().create()
+        val points: String = gsonPretty.toJson(jsonPoints)
+        saveJSON(points)
+    }
+
+    private fun saveJSON(jsonString: String) {
+        val output: Writer
+        val file = createFile()
+        output = BufferedWriter(FileWriter(file))
+        output.write(jsonString)
+        output.close()
+    }
+
+    private fun readJSON() {
+        val floatType: Type = object : TypeToken<ArrayList<ArrayList<FloatPoint>>>() {}.type
+        var filename = getFile().absolutePath
+        val reader = JsonReader(FileReader(filename))
+        val listOfPoints: ArrayList<ArrayList<FloatPoint>> = Gson().fromJson(reader, floatType)
+        for (points in listOfPoints) {
+            val path = Path()
+            for (i in points.indices) {
+                if (i == 0) {
+                    path.moveTo(points[i].x, points[i].y)
+                } else {
+                    path.lineTo(points[i].x, points[i].y)
+                }
+            }
+
+            defaultStrokes.add(path)
+        }
+    }
+
+    private fun getFile(): File {
+        val fileName = "sample.json"
+        val storageDir = getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)
+
+        return File(
+            storageDir,
+            fileName
+        )
+    }
+
+    private fun createFile(): File {
+        val fileName = "sample.json"
+        val storageDir = getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)
+        if (storageDir != null) {
+            if (!storageDir.exists()){
+                storageDir.mkdir()
+            }
+        }
+
+        return File(
+            storageDir,
+            fileName
+        )
     }
 }
---
+++ app/src/main/java/com/example/strokematching/PaintView.kt
@@ -0,0 +1,114 @@
+package com.example.strokematching
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Path
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewGroup
+
+class FloatPoint(var x: Float, var y: Float)
+class PaintView: View {
+
+    private var params: ViewGroup.LayoutParams? = null
+    private var defaultPaintBrush = Paint()
+    private var paintBrush = Paint()
+    private var path = Path()
+    private var strokes = ArrayList<ArrayList<Path>>()
+    private var currentPoints = ArrayList<FloatPoint>()
+    private var currentCounter = 0
+
+    companion object {
+        var finishCounter = 0
+        var jsonPoints = ArrayList<ArrayList<FloatPoint>>()
+        var defaultStrokes = ArrayList<Path>()
+    }
+
+    constructor(context: Context) : this(context, null) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        init()
+    }
+
+    private fun init() {
+        defaultPaintBrush.isAntiAlias = true
+        defaultPaintBrush.color = Color.BLACK
+        defaultPaintBrush.style = Paint.Style.STROKE
+        defaultPaintBrush.strokeJoin = Paint.Join.ROUND
+        defaultPaintBrush.strokeWidth = 34f
+
+        paintBrush.isAntiAlias = true
+        paintBrush.color = Color.GREEN
+        paintBrush.style = Paint.Style.STROKE
+        paintBrush.strokeJoin = Paint.Join.ROUND
+        paintBrush.strokeWidth = 34f
+
+        strokes.add(ArrayList())
+
+        params = ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        val x = event.x
+        val y = event.y
+
+        when(event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                path.moveTo(x, y)
+                strokes.last().add(path)
+                currentPoints.add(FloatPoint(x, y))
+                return true
+            }
+
+            MotionEvent.ACTION_MOVE -> {
+                path.lineTo(x, y)
+                strokes.last().add(path)
+                currentPoints.add(FloatPoint(x, y))
+            }
+
+            MotionEvent.ACTION_UP -> {
+                val cloneCurrentPoints = currentPoints.clone() as ArrayList<FloatPoint>
+//                val cloneDefaultPoints =
+
+
+                if (currentCounter == finishCounter) {
+
+                }
+
+                jsonPoints.add(cloneCurrentPoints)
+
+                strokes.add(ArrayList())
+                currentPoints.clear()
+                path = Path()
+            }
+
+            else -> return false
+        }
+
+        postInvalidate()
+        return false
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        for(stroke in defaultStrokes) {
+            canvas.drawPath(stroke, defaultPaintBrush)
+            invalidate()
+        }
+
+        for(stroke in strokes) {
+            for(path in stroke) {
+                canvas.drawPath(path, paintBrush)
+                invalidate()
+            }
+        }
+    }
+}
